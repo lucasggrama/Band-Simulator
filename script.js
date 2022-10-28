57855787e700514c6d9aac876d7ab42242b5ccdf
Lucas Grama <lucasggrama@protonmail.com>
2022-10-28 18:47:17 -0300

    teste

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//salve
 function gerarNumero(min, max) {
     return parseInt(Math.random() * (max - min) + min);
 }
+//teste
 let bandas= [
     { 
         imagem: "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2508495.jpg&f=1&nofb=1&ipt=aee8d3a84421f0fa24cf3d8998766d378de9c87eeec68426eec7232f8341051c&ipo=images')"
