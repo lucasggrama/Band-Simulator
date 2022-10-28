32ba2c96f020bfa545f0342566a6d1e4c085112b
Miguel <miguel.silva2702@hotmail.com>
2022-10-28 19:41:00 -0300

    Alterações url

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ function gerarNumero(min, max) {
 }
 let bandas= [
     { 
-        imagem: "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2508495.jpg&f=1&nofb=1&ipt=aee8d3a84421f0fa24cf3d8998766d378de9c87eeec68426eec7232f8341051c&ipo=images')"
+        imagem: "url('./imgs/ghost.png')"
     },
     {
-        imagem: ""
+        imagem: "url('./imgs/stones.png')"
     },
     {
-        imagem: ""
+        imagem: "url('./imgs/mayhem.png')"
     },
     {
-        imagem: ""
+        imagem: "url('./imgs/queen.png')"
     },
     {
-        imagem: ""
+        imagem: "url('./imgs/metallica')"
     },
     ]
 let i = gerarNumero(0, 5);
-document.body.style.backgroundImage = `url(${bandas[0].imagem})`;
+document.body.style.backgroundImage = `url(${bandas[i].imagem})`;
