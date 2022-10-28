b68fb92765abbad252434223465b062bd264c4a4
Miguel <miguel.silva2702@hotmail.com>
2022-10-28 19:02:00 -0300

    Alterações blah feita hoje

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ function gerarNumero(min, max) {
 let bandas = [
     {
         imagem: "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2508495.jpg&f=1&nofb=1&ipt=aee8d3a84421f0fa24cf3d8998766d378de9c87eeec68426eec7232f8341051c&ipo=images')"
-    }
+    },
+    {
+        imagem: ""
+    },
+    {
+        imagem: ""
+    },
+    {
+        imagem: ""
+    },
+    {
+        imagem: ""
+    },
     ]
 let i = gerarNumero(0, 5);
 document.body.style.backgroundImage = `url(${bandas[0].imagem})`;
