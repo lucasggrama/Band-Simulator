8731b1efeb5536d6e37678747ab3293f601657a7
lucasggrama <106435098+lucasggrama@users.noreply.github.com>
2022-11-04 12:46:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,40 @@
 function gerarNumero(min, max) {
     return parseInt(Math.random() * (max - min) + min);
 }
-let bandas= [
-    { 
+let bandas = [
+    {
         imagem: './imgs/ghost.png',
-        musica: 'Dance Macabre - Ghost'
+        musica: 'Dance Macabre - Ghost',
+        audio: 'audio/ghost.mp3'
     },
     {
         imagem: './imgs/stones.png',
-        musica: 'Paint it, Black - The Rolling Stones'
+        musica: 'Paint it, Black - The Rolling Stones',
+        audio: 'audio/stones.mp3'
     },
     {
         imagem: './imgs/mayhem.png',
-        musica: 'Freezing Moon - Mayhem'   
+        musica: 'Freezing Moon - Mayhem',
+        audio: 'audio/mayhem.mp3'
     },
     {
         imagem: './imgs/queen.png',
-        musica: 'Don`t Stop Me Now - Queen'
+        musica: 'Don`t Stop Me Now - Queen',
+        audio: 'audio/queen.mp3'
     },
     {
         imagem: './imgs/metallica.png',
-        musica: 'Sad But True - Metallica'
-    },
-    ]
+        musica: 'Sad But True - Metallica',
+        audio: 'audio/metallica.mp3'
+  },
+]
 
 let i = gerarNumero(0, 5);
-if(i === localStorage.getItem('imagemAleatoria')){
+if (i === localStorage.getItem('imagemAleatoria')) {
     geraNovamente();
 }
-function geraNovamente(){
-    if (i == localStorage.getItem('imagemAleatoria')){
+function geraNovamente() {
+    if (i == localStorage.getItem('imagemAleatoria')) {
         i = gerarNumero(0, 5);
         geraNovamente();
     }
@@ -40,7 +45,9 @@ geraNovamente();
 localStorage.setItem('imagemAleatoria', i);
 let imagemBackground = document.querySelector('#bodyInicial');
 let musica = document.querySelector('#musica');
+let audio = document.querySelector('audio');
 musica.innerHTML = bandas[i].musica;
+audio.src = bandas[i].audio;
 imagemBackground.style.backgroundImage = `url(${bandas[i].imagem})`;
 
 // LIGHT MODE
