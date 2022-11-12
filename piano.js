456cd4c278b3539f269b14a84e7f79c42d08ba9e
lucas <lucasggrama@protonmail.com>
2022-11-12 10:20:12 -0300

    ESTA VIVO ESTA VIVO

--- piano.js
+++ piano.js
@@ -1,7 +1,12 @@
 const teclaPiano = document.querySelectorAll('.tecla');
 function tocar(id) {
+    let tocada = document.getElementById(id);
     let url = 'piano/' + id +'.mp3'
+tocada.classList.add("tocando");
     new Audio(url).play();
+    setTimeout(() => {
+        tocada.classList.remove("tocando");
+    }, 500);
 }
 function teste(number) {
 alert(number);
@@ -9,4 +14,5 @@ alert(number);
 teclaPiano.forEach((tecla, i) => {
     const number = i < 9 ? '0' + (i + 1) : (i + 1);
     tecla.addEventListener('click', () => tocar(number))
+
 })
