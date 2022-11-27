145df779d016397a4499b8fe3bf93051494350cc
lucas <lucasggrama@protonmail.com>
2022-11-27 11:00:59 -0300

    Caneta azul + alteracoes painel de jogo

--- piano.js
+++ piano.js
@@ -8,31 +8,54 @@ const teclaPiano = document.querySelectorAll('.tecla');
 let time = 0;
 let strUsuario = [];
 let idmus = 0;
+let subtrai = document.querySelector('#subtrai');
+let soma = document.querySelector('#soma');
+let link = document.querySelector('a');
 
 let doremifa = [18, 19, 20, 21, 25, 21, 21, 25, 18, 19, 18, 19, 25, 19, 19, 25, 18, 22, 21, 20, 25, 20, 20, 18, 19, 20, 21, 25, 21, 21];
 let nonap1 = [17, 17, 18, 19, 19, 18, 17, 16, 15, 15, 16, 17, 17, 25, 16, 16, 17, 17, 18, 19, 19, 18, 17, 16, 15, 15, 16, 17, 16, 25, 15, 15];
 let jinglebell = [17, 17, 17, 25, 17, 17, 17, 25, 17, 19, 15, 16, 17, 25, 25, 25, 18, 18, 18, 25, 17, 17, 17, 25, 16, 17, 16, 17, 16, 25, 19, 17, 17, 17, 25, 17, 17, 17, 25, 17, 19, 15, 16, 25, 18, 18, 18, 25, 18, 17, 17, 25, 18, 17, 15, 14];
 let asabranca = [15, 16, 17, 25, 19, 25, 19, 25, 17, 25, 18, 25, 25, 25, 18, 15, 16, 17, 19, 25, 19, 25, 18, 17, 17, 25, 15, 15, 16, 17, 19, 25, 19, 18, 17, 15, 18, 25, 25, 25, 17, 17, 16, 16, 25];
-
+let canetaazul = [16, 19, 19, "06", 25, 25, 16, 17, 17, 16, 25, 25, 16, 19, 19, "06"];
 let placar = parseInt(localStorage.getItem('placar'));
 let dificuldade = document.querySelector('#dificuldade');
 
 // variavel para parar a musica caso perca
 let musica = [], music = [];
 let tituloMusica = document.querySelector('#botao1')
-idchange(0);
 
-tituloMusica.addEventListener('click', () => {
+/*tituloMusica.addEventListener('click', () => {
     ++idmus;
-    idmus %= 4;
+    idmus %= 5;
     idchange(idmus);
-})
+})*/
+idchange(idmus)
+soma.addEventListener('click', () => {
+    idmus = (idmus + 1) % 5;
+    idchange(idmus);
+}
+);
+subtrai.addEventListener('click', () => {
+    idmus -= 1;
+    if(idmus == -1)
+        idmus = 4;
+    idchange(idmus);
+}
+);
 
 function idchange(idm) {
     if (idm == 0) {
         musica = doremifa;
         tituloMusica.innerHTML = "Do re mi fa"
         dificuldade.innerHTML = "Dificuldade: Fácil"
+        
+        
+    }
+    else if(idm == 4)
+    {
+        musica = canetaazul;
+        tituloMusica.innerHTML = "Caneta azul";
+        dificuldade.innerHTML = "Dificuldade: 🖊️";
     }
     else if (idm == 1) {
         musica = jinglebell;
@@ -44,7 +67,7 @@ function idchange(idm) {
         tituloMusica.innerHTML = "9º Sinfonia de Bethoven";
         dificuldade.innerHTML = "Dificuldade: Médio";
     }
-    else {
+    else if(idm == 3) {
         musica = asabranca;
         tituloMusica.innerHTML = "Asa Branca"
         dificuldade.innerHTML = "Dificuldade: Difícil"
@@ -52,6 +75,9 @@ function idchange(idm) {
 }
 
 botaoJogar.addEventListener('click', () => {
+    if(idmus == -1)
+        alert("Escolha uma musica primeiro !!!")
+    else {
     menu.classList.add("none");
     pia.classList.remove("none");
 
@@ -62,6 +88,7 @@ botaoJogar.addEventListener('click', () => {
             music.push(musica[i]);
 
     play(musica.length - 1);
+    }
 })
 
 voltar.addEventListener('click', () => {
@@ -73,8 +100,8 @@ voltar.addEventListener('click', () => {
 
 let containerFimEl = document.querySelector("#fim-de-jogo");
 
-function erro() {
-    window.location.reload(true);
+function erro(id) {
+    window.location.reload(true);  
     alert("Ahh! Você Perdeu! 😫");
 }
 function fim() {
@@ -88,19 +115,33 @@ let tam;
 function tocar(id, clique) {
     if (clique == 1)
         strUsuario.push(id);
+
     let tocada = document.getElementById(id);
     let url = 'piano/' + id + '.mp3'
-    tocada.classList.add("tocando");
+    if(clique != 1)
+        tocada.classList.add("tocando");
     new Audio(url).play();
+    if(clique != 1)
     setTimeout(() => {
         tocada.classList.remove("tocando");
     }, 500);
     if (clique == 1) {
         tam = strUsuario.length - 1;
         if (strUsuario[tam] != music[tam])
+        {
+            tocada.classList.add("erro");
             setTimeout(erro, 500);
+        }
+        else {
+            tocada.classList.add("acerto");
+            setTimeout(() => {
+                tocada.classList.remove("acerto");
+            }, 500);
+        }
         if (strUsuario.length == music.length)
+        {
             setTimeout(fim, 500)
+        }
     }
 }
 function pausecomp(millis) {
